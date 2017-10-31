9faf9f1b6a44c7e63d0e868a3cfd2da8903d9db2
Beau Kinstler <git@beaukinstler.info>
2017-10-31 14:27:38 -0400

    Add better formatting after review

--- media.py
+++ media.py
@@ -1,14 +1,35 @@
-"""This module stores classes for media objects, like Movie, Video, Show"""
+"""
+This module stores classes for media objects, like Movie, Video, Show
+"""
 
 class Movie(object):
-    '''A class for a basic high-level abstraction on a movie and key web links'''
+    """
+    A class for a basic high-level abstraction on a movie and key web links
+    """
 
-    def __init__(self, title, description, poster_image_url, trailer_youtube_url):
+    def __init__(self,
+                 title,
+                 description,
+                 poster_image_url,
+                 trailer_youtube_url):
+        """
+        Initializes and instance of the Movie() class
+            :param self: 
+            :param title: 
+            :param description: 
+            :param poster_image_url: 
+            :param trailer_youtube_url: 
+        """   
         self.title = title
         self.description = description
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
 
     def getmoviesummaries(self, movie):
-        '''Returns the a title and Description of the movie'''
-        return str.format("Title: {0} - Description: {1}", movie.title, movie.description)
+        """
+        Returns the a title and Description of the movie
+        """
+        return str.format(
+            "Title: {0} - Description: {1}",
+            movie.title,
+            movie.description)
